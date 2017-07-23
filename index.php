5088be4833b87468931f3e1435dd4838f642e6e1
Ampt Design Admin <amptdesign@live.com>
2017-07-23 14:39:12 -0500

    rearranged code and cleaned it up.

--- index.php
+++ index.php
@@ -7,23 +7,6 @@
     use Serps\Core\Browser\Browser;
     use Serps\SearchEngine\Google\GoogleUrl;
     use Serps\SearchEngine\Google\NaturalResultType;
-    
-
-    $userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
-    $language = 'en-US,en;q=0.8';
-    $someProxy=null; 
-    $someCookies=null;
-    $browser = new Browser(new CurlClient(), $userAgent, $language, $someProxy, $someCookies);
-    // $browser = new Browser(new CurlClient(), $userAgent, $language);
-
-    $googleUrl = new GoogleUrl();
-    $searchTerm = 'SHIH SHUI KUNG FU';
-    $googleUrl->setSearchTerm($searchTerm);
-
-    $googleClient = new GoogleClient($browser);
-    $response = $googleClient->query($googleUrl);
-
-    $results = $response->getNaturalResults();
 
     function isKeyword($term) {
         // $arr1 = array (1=>'littlenineheaven',2=>'Shih Shui Kung Fu Temple',3=>'405 Lake Cook Road',4=>'shih-shui-deerfield');
@@ -35,40 +18,57 @@
 
     }
 
-    echo '<h3>Google words searched: ' .$searchTerm .'</h3>';
+    $userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+    $language = 'en-US';
+    $someProxy=null; 
+    $someCookies=null;
+    $browser = new Browser(new CurlClient(), $userAgent, $language, $someProxy, $someCookies);
 
-    $i=0;
-    echo '<ol>';
+    $googleUrl = new GoogleUrl();
+    $searchTerm = 'SHIH SHUI KUNG FU';
+    $googleUrl->setSearchTerm($searchTerm);
+
+    $googleClient = new GoogleClient($browser);
+    $response = $googleClient->query($googleUrl);
+
+    $results = $response->getNaturalResults();
+    
+
+  ?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Testing curl with serps</title>
+</head>
+<body>
+
+<h3>Google words searched: <?php echo $searchTerm; ?></h3>
+<ol>
+
+<?php 
+
     foreach($results as $result){
-        $i++;
-        
         if($result->is(NaturalResultType::CLASSICAL)){
-            echo '<li>' .$result->title .'<br>';
+            $title=$result->title;
             $url = $result->url;
+            echo '<li>' .$title .'<br>';
             echo '<a href="'.$url .'" target="_BLANK">' .$url .'</a></li>';
         }
     }
-    echo '</ol>';
 
     $temp = 'hotdog';
     try {if (isKeyword($temp)=='korean') {echo 'success!';};} 
     catch (Exception $e) {echo 'Caught exception: ',$e->getMessage(),"\n";}
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>Testing curl with serps</title>
-</head>
-<body>
+</ol>
 
 <?php 
 // echo phpinfo(); 
-// echo("hello");
 // echo phpversion();
-?>
-
+ ?>
 </body>
 </html>
